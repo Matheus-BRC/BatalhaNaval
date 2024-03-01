540a40ff0126607632c550bfcbcb78caa55e10f2
Matheus-BRC <matheusbatista327@gmail.com>
2024-03-01 17:02:40 -0300

    Segundo

--- Jogo.java
+++ Jogo.java
@@ -23,19 +23,22 @@ public class Jogo{
     
     private void setJogo(Tabuleiro tab1, Tabuleiro tab2){
         System.out.println("\n JOGO \n");
+        System.out.println(" Jogador1: " + (getNaviosInicial() - tab2.getNaviosAtual()) + " X " + (getNaviosInicial() - tab1.getNaviosAtual()) +" :Jogador2");
         do{
             if(tab1.getNaviosAtual() > 0) {
-                System.out.println("Jogador 1 acerte o Navio do Jogador 2:");
+                System.out.println("\nJogador 1 acerte o Navio do Jogador 2:");
                 setJogada(tab2);
+                System.out.println(" Jogador1: " + (getNaviosInicial() - tab2.getNaviosAtual()) + " X " + (getNaviosInicial() - tab1.getNaviosAtual()) +" :Jogador2");
             }
             if(tab2.getNaviosAtual() > 0) {
-                System.out.println("Jogador 2 acerte o Navio do Jogador 1:");
+                System.out.println("\nJogador 2 acerte o Navio do Jogador 1:");
                 setJogada(tab1);
+                System.out.println(" Jogador1: " + (getNaviosInicial() - tab2.getNaviosAtual()) + " X " + (getNaviosInicial() - tab1.getNaviosAtual()) +" :Jogador2");
             }
         }
         while(tab1.getNaviosAtual() > 0 && tab2.getNaviosAtual() > 0);
-        if(tab1.getNaviosAtual() == 0) System.out.print("JOGADOR 2 VENCEU!!!");
-        else System.out.print("JOGADOR 1 VENCEU!!!");
+        if(tab1.getNaviosAtual() == 0) System.out.print("\nJOGADOR 2 VENCEU!!!");
+        else System.out.print("\nJOGADOR 1 VENCEU!!!");
     }
     
     private int setJogada(Tabuleiro alvo){
